1c91883f8a06e6654ed37c85cf5d5fafb05cb1d3
Alberto Geniola <albertogeniola@gmail.com>
2021-09-26 18:01:20 +0200

    Implemented area alarm status

---
+++ elmax_api/model/alarm_status.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class AlarmArmStatus(Enum):
+    ARMED_TOTALLY = "Totale"
+    ARMED_P1 = "P1"
+    ARMED_P2 = "P2"
+    ARMED_P1_P2 = "P1+P2"
+    NOT_ARMED = "DIS"
+
+
+class AlarmStatus(Enum):
+    TRIGGERED = "in Allarme"
+    ARMED_STANDBY = "Inserita e a riposo"
+    NOT_ARMED_TRIGGERED = "non inserita e zone aperte"
+    NOT_ARMED_NOT_TRIGGERED = "non inserita e pronta all'inserimento"
--- elmax_api/model/area.py
+++ elmax_api/model/area.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from elmax_api.model.alarm_status import AlarmStatus, AlarmArmStatus
 from elmax_api.model.endpoint import DeviceEndpoint
 
 
@@ -11,25 +12,37 @@ class Area(DeviceEndpoint):
                  visible: bool,
                  index: int,
                  name: str,
-                 status: str,
-                 armed_status: str):
+                 status: AlarmStatus,
+                 armed_status: AlarmArmStatus):
         super().__init__(endpoint_id=endpoint_id, visible=visible, index=index, name=name)
         self._status = status
         self._armed_status = armed_status
 
     @property
-    def status(self) -> str:
+    def alarm_status(self) -> AlarmStatus:
+        """
+        Current alarm status.
+
+        Returns: `AlarmStatus`
+
+        """
         return self._status
 
     @property
-    def armed_status(self) -> str:
+    def armed_status(self) -> AlarmArmStatus:
+        """
+        Current alarm arm status
+
+        Returns: `AlarmArmStatus`
+
+        """
         return self._armed_status
 
     def __eq__(self, other):
         super_equals = super().__eq__(other)
         if not super_equals:
             return False
-        return self.status == other.status and self.armed_status==other.armed_status
+        return self.alarm_status == other.alarm_status and self.armed_status==other.armed_status
 
     @staticmethod
     def from_api_response(response_entry: Dict) -> 'Area':
@@ -39,7 +52,7 @@ class Area(DeviceEndpoint):
             visible=response_entry.get('visibile'),
             index=response_entry.get('indice'),
             name=response_entry.get('nome'),
-            status=response_entry.get('stato'),
-            armed_status=response_entry.get('statoInserimento')
+            status=AlarmStatus(response_entry['statoInserimento']),
+            armed_status=AlarmArmStatus(response_entry['stato'])
         )
         return area
