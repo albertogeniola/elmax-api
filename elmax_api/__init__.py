55622a2698d5590627df4e46427f5415bff675fa
Alberto Geniola <albertogeniola@gmail.com>
2021-10-28 20:06:00 +0200

    Updated version

--- elmax_api/__init__.py
+++ elmax_api/__init__.py
@@ -1,7 +1,7 @@
 """Client for the Elmax Cloud services."""
 
 __name__ = "elmax_api"
-__version__ = "0.0.1a"
+__version__ = "0.0.1"
 __license__ = "MIT"
 __url__ = "https://github.com/albertogeniola/elmax-api"
 __author__ = "Alberto Geniola"
